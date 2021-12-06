8829cc429a2b649708346fefb6161ebc636a519c
victorobrien <victorobrien@hotmail.com>
2021-12-06 10:10:07 -0300

    :white-check-mark:stripe checkout

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,19 +1,47 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { query as q } from "faunadb";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    const session = await getSession();
+    const session = await getSession({ req });
 
-    const stripeCutomer = stripe.customers.create({
-      email: session.user.email,
-      // metadata
-    });
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
 
-    const stripeCheckouSession = await stripe.checkout.sessions.create({
-      customer: (await stripeCutomer).id,
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      });
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
+
+    const StripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId, //criando cliente no stripe
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [{ price: "price_1K2cGTKtsYIO6gndp9SCWosk", quantity: 1 }],
@@ -23,7 +51,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       cancel_url: process.env.STRIPE_CANCEL_URL,
     });
 
-    return res.status(200).json({ sessionId: stripeCheckouSession.id });
+    return res.status(200).json({ sessionId: StripeCheckoutSession.id });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not allowed");
