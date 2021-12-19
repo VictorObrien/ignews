cf26cd7ff87effee6643e3ad0ddae7656ca4defa
victorobrien <victorobrien@hotmail.com>
2021-12-19 15:43:31 -0300

    :white_check_mark: ActiveLink component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { SignInButton } from "../SignInButton";
 
 import logoSvg from "../../../public/images/logo.svg";
 
 import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
-  const { asPath } = useRouter();
-
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src={logoSvg} alt="ignews seu blog de tecnologia" />
         <nav>
-          <Link href="/">
-            <a className={asPath === "/" ? styles.active : ""}>Home</a>
-          </Link>
-          <Link href="/posts">
-            <a className={asPath === "/posts" ? styles.active : ""}>Posts</a>
-          </Link>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
