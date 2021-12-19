e96c66af2fb3ca59475e5cc1a0527e270d0b7d41
victorobrien <victorobrien@hotmail.com>
2021-12-19 15:26:45 -0300

    :white_check_mark: routing links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { SignInButton } from "../SignInButton";
 
-import logoSvg from '../../../public/images/logo.svg';
+import logoSvg from "../../../public/images/logo.svg";
 
 import styles from "./styles.module.scss";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src={logoSvg}  alt="ignews seu blog de tecnologia" />
+        <Image src={logoSvg} alt="ignews seu blog de tecnologia" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={asPath === "/" ? styles.active : ""}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a className={asPath === "/posts" ? styles.active : ""}>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
